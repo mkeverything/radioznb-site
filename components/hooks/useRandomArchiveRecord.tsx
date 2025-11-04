46c10c4770d4c952823500dda54dc21b66b1f2fe
Anatoly <yatskovanatoly@gmail.com>
2025-11-04 17:39:32 +0300

    style: format

--- components/hooks/useRandomArchiveRecord.tsx
+++ components/hooks/useRandomArchiveRecord.tsx
@@ -1,25 +1,24 @@
-import { getRecordings } from "@/lib/actions";
-import { useQuery } from "@tanstack/react-query";
-
-import { Stream } from "../PlayerContext";
+import { getRecordings } from '@/lib/actions'
+import { useQuery } from '@tanstack/react-query'
+import { Stream } from '../PlayerContext'
 
 const useRandomArchiveStream = (): Stream => {
-  const { data: randomRec } = useQuery({
-    queryKey: ["recordings", "random"],
-    queryFn: async () => {
-      const allRecs = await getRecordings();
-      if (allRecs.success && allRecs.data && allRecs.data.length > 0) {
-        const index = Math.floor(Math.random() * allRecs.data.length);
-        return allRecs.data[index];
-      }
-      return undefined;
-    },
-  });
+	const { data: randomRec } = useQuery({
+		queryKey: ['recordings', 'random'],
+		queryFn: async () => {
+			const allRecs = await getRecordings()
+			if (allRecs.success && allRecs.data && allRecs.data.length > 0) {
+				const index = Math.floor(Math.random() * allRecs.data.length)
+				return allRecs.data[index]
+			}
+			return undefined
+		},
+	})
 
-  return {
-    src: randomRec ? randomRec.fileUrl : "",
-    title: randomRec ? randomRec.episodeTitle : "",
-  };
-};
+	return {
+		src: randomRec ? randomRec.fileUrl : '',
+		title: randomRec ? randomRec.episodeTitle : '',
+	}
+}
 
-export default useRandomArchiveStream;
+export default useRandomArchiveStream
