36947b7d3e2994f5ea0d2ca570175a49dd5aa5f0
Anatoly <yatskovanatoly@gmail.com>
2025-11-19 00:35:18 +0300

    style: rename FlexContainer component to Flex

--- components/FlexContainer.tsx
+++ components/FlexContainer.tsx
@@ -1,10 +1,10 @@
 import { FC, PropsWithChildren } from 'react'
 
-const FlexContainer: FC<{ className?: string } & PropsWithChildren> = ({
+const Flex: FC<{ className?: string } & PropsWithChildren> = ({
 	children,
 	className,
 }) => {
-	return <div className={`flex flex-wrap  gap-2  ${className}`}>{children}</div>
+	return <div className={`flex flex-wrap gap-2 ${className}`}>{children}</div>
 }
 
-export default FlexContainer
+export default Flex
