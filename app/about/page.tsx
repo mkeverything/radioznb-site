44c25a58f7bc02d7be970f164cb97d58eb2ecdcb
Anatoly <yatskovanatoly@gmail.com>
2026-04-07 19:07:17 +0300

    feat: version in /about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,21 +1,19 @@
 import HomeButton from "@/components/HomeButton"
 import { stationRestUrl } from "@/lib/radioStation"
+import packageJson from "@/package.json"
 import Image from "next/image"
 import Link from "next/link"
 
 const Page = async () => {
-  const request = await fetch(
-    stationRestUrl,
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.NEXT_PUBLIC_RADIOZNB_API_KEY}`,
-      },
+  const request = await fetch(stationRestUrl, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_RADIOZNB_API_KEY}`,
     },
-  )
+  })
   const data = await request.json()
 
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex size-full flex-col">
       <HomeButton />
       <div className="flex flex-col items-center">
         <p className="max-w-lg p-4 text-xl text-pretty sm:p-12">
@@ -30,6 +28,19 @@ const Page = async () => {
             alt="telegram"
           />
         </Link>
+      </div>
+      <div className="fixed bottom-32 sm:bottom-16 right-0 pointer-events-none flex flex-col items-end p-8 text-xs opacity-10">
+        <p className="pointer-events-auto">{packageJson.version}</p>
+        <p className="pointer-events-auto">
+          <Link
+            href="https://mkeverything.ru"
+            className="underline underline-offset-2 hover:opacity-100"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            mkeverything
+          </Link>
+        </p>
       </div>
     </div>
   )
